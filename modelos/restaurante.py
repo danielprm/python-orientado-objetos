dbb684ba8666a67d5901ab5ac992e1f92cd59a9a
daniel <danielprmjob@gmail.com>
2025-06-24 16:19:01 -0300

    aprendendo ojetos e como manipular dados no objeto

---
+++ modelos/restaurante.py
@@ -0,0 +1,15 @@
+class Restaurante:
+    nome = ''
+    categoria = ''
+    ativo = False
+
+restaurante_praca = Restaurante()
+restaurante_praca.nome = 'praca'
+restaurante_praca.categoria = 'gourmet'
+
+restaurante_pizza = Restaurante()
+
+Restaurantes = [restaurante_praca,
+restaurante_pizza]
+
+print(Restaurantes)
